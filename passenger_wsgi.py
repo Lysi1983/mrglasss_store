6a764281e6ca087be39ac87b4ab9fd996c6e68df
Lyudmil Karaivanov <109275044+Lysi1983@users.noreply.github.com>
2025-10-13 12:01:52 +0300

    Update passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,7 +1,12 @@
 import sys
 import os
 
-sys.path.insert(0, '/home/mrglass2/mrglass_store')
+# Това е добра практика. Гарантира, че модулите в текущата
+# директория (като твоя 'app.py') ще бъдат намерени.
+sys.path.insert(0, os.path.dirname(__file__))
 
-from wsgi import application
+# Импортираме Flask обекта 'app' от твоя основен файл 'app.py'
+# и го преименуваме на 'application', както очаква Passenger.
+from app import app as application
+
 
